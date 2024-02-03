3308d38f9da6d1c0e305a8010fc5eb5044539af5
tfkhdyt <me@tfkhdyt.my.id>
2024-02-03 14:37:29 +0700

    feat(api): add testing route for saweria webhook

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -21,6 +22,16 @@ func main() {
 	app.Use(cors.New())
 
 	route.RegisterScoreRoute(app)
+	app.Post("/callback/saweria", func(c *fiber.Ctx) error {
+		if c.Query("secret") != os.Getenv("SAWERIA_SECRET") {
+			fmt.Println("error")
+			return fiber.NewError(fiber.StatusUnauthorized, "invalid secret")
+		}
+
+		fmt.Println(string(c.Body()))
+
+		return c.Status(fiber.StatusCreated).SendString("ok")
+	})
 
 	appPort := os.Getenv("APP_PORT")
 	if err := app.Listen(":" + appPort); err != nil {
