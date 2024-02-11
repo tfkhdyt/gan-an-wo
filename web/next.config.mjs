f07938a94f659537c4756464c9d59a2fb54e8bfe
tfkhdyt <tfkhdyt@proton.me>
2024-02-11 08:05:08 +0700

    build(docker): optimize docker image

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.(mp3)$/,
-            use: {
-              loader: 'file-loader',
-              options: {
-                publicPath: '/_next/static/sounds/',
-                outputPath: 'static/sounds/',
-                name: '[name].[ext]',
-                esModule: false,
-              },
-            },
-          });
-      return config;
-    },
-  }
+	// webpack(config, options) {
+	//     config.module.rules.push({
+	//         test: /\.(mp3)$/,
+	//         use: {
+	//           loader: 'file-loader',
+	//           options: {
+	//             publicPath: '/_next/static/sounds/',
+	//             outputPath: 'static/sounds/',
+	//             name: '[name].[ext]',
+	//             esModule: false,
+	//           },
+	//         },
+	//       });
+	//   return config;
+	// },
+	output: 'standalone',
+};
 
 export default nextConfig;
